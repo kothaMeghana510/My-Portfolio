e4889ee59f8fc8a3f2d86583858f4a463e856701
kothaMeghana510 <meghanakotha105@gmail.com>
2025-10-02 21:15:49 +0530

    Removed Resume in Portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,8 +6,8 @@ function Contact(){
                 <div className="media">
                     <i class="fa-solid fa-envelope contact-icon"></i>
                     <a href="mailto:meghanakotha105@gmail.com" target="_blank" rel="noopener noreferrer">
-                        <p className="label">Gmail</p>
-                    </a>
+                        <p className="label">Gmail</p> 
+                    </a> 
                 </div>
                 <div className="media">
                     <i class="fa-brands fa-linkedin contact-icon"></i> 
@@ -19,12 +19,6 @@ function Contact(){
                     <i class="fab fa-github contact-icon"></i>
                     <a href="https://github.com/kothaMeghana510" target="_blank" rel="noopener noreferrer">
                         <p className="label">Github</p>
-                    </a>
-                </div>
-                <div className="media">
-                    <i class="fa-solid fa-file contact-icon"></i>
-                    <a href={`${process.env.PUBLIC_URL}/Kotha-Meghana-Frontend-Developer-Resume.pdf`}  target="_blank" rel="noopener noreferrer">
-                        <p className="label">View My Resume</p>
                     </a>
                 </div>
             </div>
